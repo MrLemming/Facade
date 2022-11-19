d6898b72248abb35140b534cf9cfc4cdd7c2e1fc
Aleksey Esin <aesin87@mail.ru>
2022-11-19 20:17:54 +0300

    Minor fix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,10 @@
 package org.example;
 
 public class Main {
-    public static final String A = "11001"; // A = 25
-    public static final String B = "101"; // B = 5
 
     public static void main(String[] args) {
         BinOps binOps = new BinOps();
-        System.out.println(binOps.sum(A, B));
-        System.out.println(binOps.mult(A, B));
+        System.out.println(binOps.sum("11001", "101"));
+        System.out.println(binOps.mult("11001", "101"));
     }
 }
